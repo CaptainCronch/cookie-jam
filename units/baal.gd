86cfff57b011d94564dea5c822561694bdba172c
CaptainCronch <62529272+CaptainCronch@users.noreply.github.com>
2026-03-29 23:52:46 -0300

    added start of fire and stomp logic

--- units/baal.gd
+++ units/baal.gd
@@ -3,43 +3,46 @@ class_name Baal
 
 @export var default_speed := 1000.0
 @export var acceleration := 5.0
-@export var idle_speed := 50.0
 
-@export var separate_area: Area2D
-@export var separate_collider: CollisionShape2D
-@export var aggro_area: Area2D
 @export var sprite: Sprite2D
 @export var animator: AnimationPlayer
 @export var attack_audio: AudioStreamPlayer2D
-@export var fire_blast: CPUParticles2D
-@export var left: Marker2D
-@export var right: Marker2D
+@export var stomp_audio: AudioStreamPlayer2D
+@export var fire_blast_left: CPUParticles2D
+@export var fire_blast_right: CPUParticles2D
+@export var fire_area: Area2D
+@export var fire_collider_left: CollisionShape2D
+@export var fire_collider_right: CollisionShape2D
+@export var step_area: Area2D
 
 var speed := default_speed
 var direction := Vector2()
 var last_direction := 1.0
-
-
-#func _ready() -> void:
-	#Global.baal = self
+var shooting := false
 
 
 func _process(_delta: float) -> void:
 	direction = Input.get_vector("a", "d", "w", "s")
 	if direction.x != 0.0: last_direction = direction.x
-	
-	fire_blast.emitting = Input.is_action_pressed("mouse1")
+	shooting = Input.is_action_pressed("mouse1")
 
 
 func _physics_process(delta: float) -> void:
+	check_fire()
+	check_stomp()
+	
+	fire_blast_right.emitting = false
+	fire_blast_left.emitting = false
+	fire_collider_right.disabled = true
+	fire_collider_left.disabled = true
 	if last_direction > 0.0: 
 		sprite.flip_h = true
-		fire_blast.position = right.position
-		fire_blast.direction = Vector2(1, 1)
+		fire_blast_right.emitting = shooting
+		fire_collider_right.disabled = !shooting
 	else:
 		sprite.flip_h = false
-		fire_blast.position = left.position
-		fire_blast.direction = Vector2(-1, 1)
+		fire_blast_left.emitting = shooting
+		fire_collider_left.disabled = !shooting
 	
 	#if distance < close_buffer:
 		#if not animator.current_animation == "idle": animator.play("idle")
@@ -52,10 +55,23 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
-func check_interaction() -> void:
-	for area in separate_area.get_overlapping_areas():
+func check_fire() -> void:
+	for area in fire_area.get_overlapping_areas():
 		var parent := area.get_parent()
 		if (parent is Enemy or parent is Victim):
 			pass
 		elif parent is Unit:
 			pass
+		elif area is Interactable:
+			pass
+
+
+func check_stomp() -> void:
+	for area in step_area.get_overlapping_areas():
+		var parent := area.get_parent()
+		if (parent is Enemy or parent is Victim):
+			pass
+		elif parent is Unit:
+			pass
+		elif area is Interactable:
+			area.die()
